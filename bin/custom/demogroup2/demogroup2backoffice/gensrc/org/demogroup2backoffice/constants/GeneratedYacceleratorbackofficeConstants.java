2dba3da0794d96fde329f6a03a5de5d8f399817e
nbarkah <nurhaibarkah9@gmail.com>
2022-11-30 18:52:13 +0700

    new project group 2

---
+++ bin/custom/demogroup2/demogroup2backoffice/gensrc/org/demogroup2backoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 30, 2022, 6:29:11 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.demogroup2backoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "demogroup2backoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bin/custom/demogroup2/demogroup2facades/gensrc/org/demo/facades/constants/GeneratedDemogroup2FacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 30, 2022, 6:29:11 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.demo.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedDemogroup2FacadesConstants
+{
+	public static final String EXTENSIONNAME = "demogroup2facades";
+	
+	protected GeneratedDemogroup2FacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
