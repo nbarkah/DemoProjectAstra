0d58fe31104b78ba4d60ead126abced3ec0620c2
Anggi-anggoro <anggi.anggoro26@gmail.com>
2022-12-01 07:42:32 +0700

    Apa ajalah gausaha pke petik langsung aja

--- bin/custom/demogroup2/demogroup2core/src/org/demo/core/dao/Eksperimen.java
+++ bin/custom/demogroup2/demogroup2core/src/org/demo/core/dao/Eksperimen.java
@@ -2,6 +2,7 @@ package org.demo.core.dao;
 
 public class Eksperimen {
     public static void main(String[] args) {
-        System.out.println("ahuy");
+        System.out.println("xixixixi");
+        System.out.println("cok");
     }
 }
