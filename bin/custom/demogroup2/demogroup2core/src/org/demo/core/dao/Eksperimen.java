cf661b4869794e1153a702707f07ace3f7663f64
nbarkah <nurhaibarkah9@gmail.com>
2022-12-01 07:40:33 +0700

    eksperimen lagi ver5

--- bin/custom/demogroup2/demogroup2core/src/org/demo/core/dao/Eksperimen.java
+++ bin/custom/demogroup2/demogroup2core/src/org/demo/core/dao/Eksperimen.java
@@ -3,5 +3,8 @@ package org.demo.core.dao;
 public class Eksperimen {
     public static void main(String[] args) {
         System.out.println("ahuy");
+        System.out.println("ahuy");
+        System.out.println("ahuy");
+        System.out.println("ahuy");
     }
 }
