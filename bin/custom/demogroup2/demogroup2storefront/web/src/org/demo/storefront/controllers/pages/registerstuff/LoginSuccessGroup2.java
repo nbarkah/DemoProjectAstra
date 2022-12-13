1cb876d295e3aee0208af1c27193cd862c87f0d7
Anggi-anggoro <anggi.anggoro26@gmail.com>
2022-12-13 16:18:32 +0700

    Register

--- bin/custom/demogroup2/demogroup2storefront/web/src/org/demo/storefront/controllers/pages/registerstuff/LoginSuccessGroup2.java
+++
@@ -1,7 +0,0 @@
-//package org.demo.storefront.controllers.pages.registerstuff;
-//
-//import de.hybris.platform.basecommerce.model.site.BaseSiteModel;
-//import de.hybris.platform.commerceservices.event.AbstractCommerceUserEvent;
-//
-//public class LoginSuccessGroup2 extends AbstractCommerceGroup2<BaseSiteModel> {
-//}
