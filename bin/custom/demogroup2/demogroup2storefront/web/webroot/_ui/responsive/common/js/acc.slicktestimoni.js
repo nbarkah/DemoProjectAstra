eadf21c8bd5fd2f9f152c5f935bdcb2239a3a828
dwickydarmawan22 <dwickydarmawan22@gmail.com>
2022-12-14 12:47:43 +0700

    update acc.slicktestimoni.js

--- bin/custom/demogroup2/demogroup2storefront/web/webroot/_ui/responsive/common/js/acc.slicktestimoni.js
+++ bin/custom/demogroup2/demogroup2storefront/web/webroot/_ui/responsive/common/js/acc.slicktestimoni.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
     $(".homepage-testimoni--slick").slick({
-      arrows: false,
+
       infinite: true,
       speed: 300,
       slidesToShow: 3,
+      responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  arrows: false,
+                  slidesToShow: 2,
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  arrows: false,
+                  slidesToShow: 1,
+                }
+              }
+        ]
     });
 });
 
